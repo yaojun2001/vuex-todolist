4a7a0acf2df69ff7c1d520a1642ec4c3a6967440
yaojun <yaojun012022@163.com>
2022-07-29 13:07:22 +0800

    移动端项目：vuex-todolist初始化完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible' // 引入flexible.js 设置根标签字体大小，做移动端的适配
 
 Vue.config.productionTip = false
 
